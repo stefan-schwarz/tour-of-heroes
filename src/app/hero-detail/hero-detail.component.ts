3e9465db1aba921459bbe296b488e02677657c68
stefan-schwarz <mail.stefanschwarz@web.de>
2022-02-10 17:04:21 +0100

    done add navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { Location } from '@angular/common'
+import { HeroService } from '../hero.service'
 import { Hero } from '../hero';
 
 @Component({
@@ -11,11 +14,25 @@ export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
 
   
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
   
   ngOnInit(): void {
     // console.log("the detail hero ->" + this.hero);
+    this.getHero();
   }
   
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 
 }
