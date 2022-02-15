05908b8278e181c94baef26e034d3c15b8e83d23
stefan-schwarz <mail.stefanschwarz@web.de>
2022-02-15 16:38:12 +0100

    start template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { DrivenFormComponent } from './driven-form/driven-form.component';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    DrivenFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/driven-form/driven-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-driven-form',
+  templateUrl: './driven-form.component.html',
+  styleUrls: ['./driven-form.component.css']
+})
+export class DrivenFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
