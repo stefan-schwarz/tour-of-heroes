889764b4be3cb8050fc6cebd0ca1109582dfba81
stefan-schwarz <mail.stefanschwarz@web.de>
2022-02-18 11:19:46 +0100

    done attribute directives

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,12 +6,13 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class HighlightDirective {
   @Input() appHighlight = '';
+  @Input() defaultColor = '';
 
   constructor(private el: ElementRef) {
   }
   
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight(this.appHighlight || 'red ');
+    this.highlight(this.appHighlight || this.defaultColor || 'red ');
   }
   
   @HostListener('mouseleave') onMouseLeave() {
